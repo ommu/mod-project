34d82a388e7d34d4566180f8f52d567e478e0527
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-06-20 04:03:46 +0700

    commit

--- modules/project/controllers/o/AdminController.php
+++ modules/project/controllers/o/AdminController.php
@@ -212,7 +212,7 @@ class AdminController extends Controller
 				$jsonError = CActiveForm::validate($model);
 				if(strlen($jsonError) > 2) {
 					$errors = $model->getErrors();
-					$summary['msg'] = "<div class='errorSummary'><strong>Please fix the following input errors:</strong>";
+					$summary['msg'] = "<div class='errorSummary'><strong>".Yii::t('phrase', 'Please fix the following input errors:')."</strong>";
 					$summary['msg'] .= "<ul>";
 					foreach($errors as $key => $value) {
 						$summary['msg'] .= "<li>{$value[0]}</li>";
