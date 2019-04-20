d8dec5ae45c7fbb3f7f0500dd55f6e2d3847d97b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-04-20 12:42:43 +0700

    drop url merge in admin_manage

--- views/data/tag/admin_manage.php
+++ views/data/tag/admin_manage.php
@@ -18,7 +18,6 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use app\components\widgets\GridView;
 use yii\widgets\Pjax;
-use yii\helpers\ArrayHelper;
 use yii\widgets\DetailView;
 use ommu\project\models\Projects;
 
@@ -103,11 +102,11 @@ array_push($columnData, [
 	],
 	'buttons' => [
 		'view' => function ($url, $model, $key) {
-			$url = Url::to(ArrayHelper::merge(['view', 'id'=>$model->primaryKey], Yii::$app->request->get()));
+			$url = Url::to(['view', 'id'=>$model->primaryKey]);
 			return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, ['title' => Yii::t('app', 'Detail Tag')]);
 		},
 		'update' => function ($url, $model, $key) {
-			$url = Url::to(ArrayHelper::merge(['update', 'id'=>$model->primaryKey], Yii::$app->request->get()));
+			$url = Url::to(['update', 'id'=>$model->primaryKey]);
 			return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, ['title' => Yii::t('app', 'Update Tag')]);
 		},
 		'delete' => function ($url, $model, $key) {
