18eb68e1f3b0ed98b83fb4e568a0d6b7346a7187
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-09 23:46:37 +0700

    fixed submitButton on field form

--- views/setting/admin/admin_index.php
+++ views/setting/admin/admin_index.php
@@ -31,7 +31,7 @@ $this->params['menu']['content'] = [
 ?>
 
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12 col-12">
+	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div class="x_panel">
 			<div class="x_title">
 				<?php if($this->params['menu']['content']):
@@ -86,7 +86,7 @@ echo GridView::widget([
 </div>
 
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12 col-12">
+	<div class="col-md-12 col-sm-12 col-xs-12">
 		<?php echo \app\components\widgets\Alert::widget(['closeButton'=>false]); ?>
 
 		<div class="x_panel">
