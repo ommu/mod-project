6e0174947f6cde0eb2f36a236815e9c6dc33a88c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 18:24:03 +0700

    modify author

--- models/ProjectCategory.php
+++ models/ProjectCategory.php
@@ -2,7 +2,7 @@
 /**
  * ProjectCategory
  *
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2013 Ommu Platform (www.ommu.co)
  * @link https://bitbucket.org/ommu/project
--- views/o/admin/admin_manage.php
+++ views/o/admin/admin_manage.php
@@ -4,7 +4,7 @@
  * @var $this AdminController
  * @var $model Projects
  *
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2013 Ommu Platform (www.ommu.co)
  * @link https://bitbucket.org/ommu/project
